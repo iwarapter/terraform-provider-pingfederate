5a0d5f0e710b239a06dcdb6811a391104d035596
Iain Adams <iwarapter@hotmail.com>
2020-05-19 14:46:49 +0100

    add sp authentication policy contract mapping

---
+++ pingfederate/resource_pingfederate_sp_authentication_policy_contract_mapping.go
@@ -0,0 +1,210 @@
+package pingfederate
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	pf "github.com/iwarapter/pingfederate-sdk-go/pingfederate"
+)
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResource() *schema.Resource {
+	return &schema.Resource{
+		Create: resourcePingFederateSpAuthenticationPolicyContractMappingResourceCreate,
+		Read:   resourcePingFederateSpAuthenticationPolicyContractMappingResourceRead,
+		Update: resourcePingFederateSpAuthenticationPolicyContractMappingResourceUpdate,
+		Delete: resourcePingFederateSpAuthenticationPolicyContractMappingResourceDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: resourcePingFederateSpAuthenticationPolicyContractMappingResourceSchema(),
+	}
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"bypass_external_validation": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Description: "External validation will be bypassed when set to true. Default to false.",
+			Default:     false,
+		},
+		"ldap_attribute_source": {
+			Type:     schema.TypeList,
+			Optional: true,
+			Elem:     resourceLdapAttributeSource(),
+		},
+		"jdbc_attribute_source": {
+			Type:     schema.TypeList,
+			Optional: true,
+			Elem:     resourceJdbcAttributeSource(),
+		},
+		"custom_attribute_source": {
+			Type:     schema.TypeList,
+			Optional: true,
+			Elem:     resourceCustomAttributeSource(),
+		},
+		"attribute_contract_fulfillment": {
+			Type:     schema.TypeSet,
+			Required: true,
+			Elem:     resourceAttributeFulfillmentValue(),
+		},
+		"source_id": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"issuance_criteria": {
+			Type:     schema.TypeList,
+			Optional: true,
+			MaxItems: 1,
+			Elem:     resourceIssuanceCriteria(),
+		},
+		"target_id": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"default_target_resource": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"license_connection_group_assignment": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+	}
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceCreate(d *schema.ResourceData, m interface{}) error {
+	svc := m.(*pf.PfClient).SpAuthenticationPolicyContractMappings
+	input := pf.CreateApcToSpAdapterMappingInput{
+		Body:                     *resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData(d),
+		BypassExternalValidation: Bool(d.Get("bypass_external_validation").(bool)),
+	}
+	result, _, err := svc.CreateApcToSpAdapterMapping(&input)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	d.SetId(*result.Id)
+	return resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadResult(d, result)
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceRead(d *schema.ResourceData, m interface{}) error {
+	svc := m.(*pf.PfClient).SpAuthenticationPolicyContractMappings
+	input := pf.GetApcToSpAdapterMappingByIdInput{
+		Id: d.Id(),
+	}
+	result, _, err := svc.GetApcToSpAdapterMappingById(&input)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	return resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadResult(d, result)
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceUpdate(d *schema.ResourceData, m interface{}) error {
+	svc := m.(*pf.PfClient).SpAuthenticationPolicyContractMappings
+	input := pf.UpdateApcToSpAdapterMappingByIdInput{
+		Id:                       d.Id(),
+		Body:                     *resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData(d),
+		BypassExternalValidation: Bool(d.Get("bypass_external_validation").(bool)),
+	}
+	result, _, err := svc.UpdateApcToSpAdapterMappingById(&input)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	return resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadResult(d, result)
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceDelete(d *schema.ResourceData, m interface{}) error {
+	svc := m.(*pf.PfClient).SpAuthenticationPolicyContractMappings
+	input := pf.DeleteApcToSpAdapterMappingByIdInput{
+		Id: d.Id(),
+	}
+	_, _, err := svc.DeleteApcToSpAdapterMappingById(&input)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	return nil
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadResult(d *schema.ResourceData, rv *pf.ApcToSpAdapterMapping) (err error) {
+	setResourceDataString(d, "source_id", rv.SourceId)
+	setResourceDataString(d, "target_id", rv.TargetId)
+	setResourceDataString(d, "default_target_resource", rv.DefaultTargetResource)
+	setResourceDataString(d, "license_connection_group_assignment", rv.LicenseConnectionGroupAssignment)
+
+	if rv.AttributeContractFulfillment != nil {
+		if err = d.Set("attribute_contract_fulfillment", flattenMapOfAttributeFulfillmentValue(rv.AttributeContractFulfillment)); err != nil {
+			return err
+		}
+	}
+	if rv.IssuanceCriteria != nil && (rv.IssuanceCriteria.ExpressionCriteria != nil && rv.IssuanceCriteria.ConditionalCriteria != nil) {
+		if err = d.Set("issuance_criteria", flattenIssuanceCriteria(rv.IssuanceCriteria)); err != nil {
+			return err
+		}
+	}
+
+	if *rv.AttributeSources != nil && len(*rv.AttributeSources) > 0 {
+		var ldapAttributes []interface{}
+		var jdbcAttributes []interface{}
+		var customAttributes []interface{}
+		for _, v := range *rv.AttributeSources {
+			switch *v.Type {
+			case "LDAP":
+				ldapAttributes = append(ldapAttributes, flattenLdapAttributeSource(&v.LdapAttributeSource))
+				break
+			case "JDBC":
+				jdbcAttributes = append(jdbcAttributes, flattenJdbcAttributeSource(v))
+				break
+			case "CUSTOM":
+				customAttributes = append(customAttributes, flattenCustomAttributeSource(&v.CustomAttributeSource))
+				break
+			}
+		}
+		if ldapAttributes != nil && len(ldapAttributes) > 0 {
+			if err = d.Set("ldap_attribute_source", ldapAttributes); err != nil {
+				return err
+			}
+		}
+		if jdbcAttributes != nil && len(jdbcAttributes) > 0 {
+			if err = d.Set("jdbc_attribute_source", jdbcAttributes); err != nil {
+				return err
+			}
+		}
+		if customAttributes != nil && len(customAttributes) > 0 {
+			if err = d.Set("custom_attribute_source", customAttributes); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData(d *schema.ResourceData) *pf.ApcToSpAdapterMapping {
+	mapping := &pf.ApcToSpAdapterMapping{
+		Id:                           String(d.Id()),
+		SourceId:                     String(d.Get("source_id").(string)),
+		TargetId:                     String(d.Get("target_id").(string)),
+		AttributeContractFulfillment: expandMapOfAttributeFulfillmentValue(d.Get("attribute_contract_fulfillment").(*schema.Set).List()),
+		AttributeSources:             &[]*pf.AttributeSource{},
+	}
+	if v, ok := d.GetOk("issuance_criteria"); ok {
+		mapping.IssuanceCriteria = expandIssuanceCriteria(v.([]interface{}))
+	}
+	if v, ok := d.GetOk("ldap_attribute_source"); ok && len(v.([]interface{})) > 0 {
+		*mapping.AttributeSources = append(*mapping.AttributeSources, *expandLdapAttributeSource(v.([]interface{}))...)
+	}
+	if v, ok := d.GetOk("jdbc_attribute_source"); ok && len(v.([]interface{})) > 0 {
+		*mapping.AttributeSources = append(*mapping.AttributeSources, *expandJdbcAttributeSource(v.([]interface{}))...)
+	}
+	if v, ok := d.GetOk("custom_attribute_source"); ok && len(v.([]interface{})) > 0 {
+		*mapping.AttributeSources = append(*mapping.AttributeSources, *expandCustomAttributeSource(v.([]interface{}))...)
+	}
+	if v, ok := d.GetOk("default_target_resource"); ok {
+		mapping.DefaultTargetResource = String(v.(string))
+	}
+	if v, ok := d.GetOk("license_connection_group_assignment"); ok {
+		mapping.LicenseConnectionGroupAssignment = String(v.(string))
+	}
+	return mapping
+}
---
+++ pingfederate/resource_pingfederate_sp_authentication_policy_contract_mapping_test.go
@@ -0,0 +1,360 @@
+package pingfederate
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+
+	pf "github.com/iwarapter/pingfederate-sdk-go/pingfederate"
+)
+
+func TestAccPingFederateSpAuthenticationPolicyContractMapping(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPingFederateSpAuthenticationPolicyContractMappingDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPingFederateSpAuthenticationPolicyContractMappingConfig("foo"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPingFederateSpAuthenticationPolicyContractMappingExists("pingfederate_sp_authentication_policy_contract_mapping.demo"),
+					//testAccCheckPingFederateSpAuthenticationPolicyContractMappingAttributes(),
+				),
+			},
+			{
+				Config: testAccPingFederateSpAuthenticationPolicyContractMappingConfig("bar"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckPingFederateSpAuthenticationPolicyContractMappingExists("pingfederate_sp_authentication_policy_contract_mapping.demo"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckPingFederateSpAuthenticationPolicyContractMappingDestroy(s *terraform.State) error {
+	return nil
+}
+
+func testAccPingFederateSpAuthenticationPolicyContractMappingConfig(configUpdate string) string {
+	return fmt.Sprintf(`resource "pingfederate_sp_authentication_policy_contract_mapping" "demo" {
+	source_id = pingfederate_authentication_policy_contract.demo.id
+	target_id = pingfederate_sp_adapter.demo.id
+    attribute_contract_fulfillment {
+      key_name = "subject"
+      source {
+        type = "AUTHENTICATION_POLICY_CONTRACT"
+      }
+      value = "subject"
+    }
+	default_target_resource = "https://%s"
+}
+
+resource "pingfederate_authentication_policy_contract" "demo" {
+  name = "spadaptertest2"
+  core_attributes = ["subject"]
+  extended_attributes = ["foo", "email"]
+}
+
+resource "pingfederate_sp_adapter" "demo" {
+  name = "spadaptertest2"
+  sp_adapter_id = "spadaptertest2"
+  plugin_descriptor_ref {
+    id = "com.pingidentity.adapters.opentoken.SpAuthnAdapter"
+  }
+
+  configuration {
+    sensitive_fields {
+      name  = "Password"
+      value = "Secret123"
+    }
+    sensitive_fields {
+      name  = "Confirm Password"
+      value = "Secret123"
+    }
+    fields {
+      name  = "Account Link Service"
+      value = ""
+    }
+    fields {
+      name  = "Authentication Service"
+      value = ""
+    }
+    fields {
+      name  = "Cipher Suite"
+      value = "2"
+    }
+    fields {
+      name  = "Cookie Domain"
+      value = ""
+    }
+    fields {
+      name  = "Cookie Path"
+      value = "/"
+    }
+    fields {
+      name  = "Force SunJCE Provider"
+      value = "false"
+    }
+    fields {
+      name  = "HTTP Only Flag"
+      value = "true"
+    }
+    fields {
+      name  = "Logout Service"
+      value = ""
+    }
+    fields {
+      name  = "Not Before Tolerance"
+      value = "0"
+    }
+    fields {
+      name  = "Obfuscate Password"
+      value = "true"
+    }
+    fields {
+      name  = "Secure Cookie"
+      value = "false"
+    }
+    fields {
+      name  = "Send Extended Attributes"
+      value = ""
+    }
+    fields {
+      name  = "Send Subject as Query Parameter"
+      value = "false"
+    }
+    fields {
+      name  = "Session Cookie"
+      value = "false"
+    }
+    fields {
+      name  = "Session Lifetime"
+      value = "43200"
+    }
+    fields {
+      name  = "Skip Trimming of Trailing Backslashes"
+      value = "false"
+    }
+    fields {
+      name  = "Subject Query Parameter                 "
+      value = ""
+    }
+    fields {
+      name  = "Token Lifetime"
+      value = "300"
+    }
+    fields {
+      name  = "Token Name"
+      value = "opentoken"
+    }
+    fields {
+      name  = "Transport Mode"
+      value = "2"
+    }
+    fields {
+      name  = "URL Encode Cookie Values"
+      value = "true"
+    }
+    fields {
+      name  = "Use Verbose Error Messages"
+      value = "false"
+    }
+
+  }
+
+  attribute_contract {
+    core_attributes = [ "subject" ]
+  }
+  
+  target_application_info {
+	application_name = "foo"
+	application_icon_url = "https://bar"
+  }
+}`, configUpdate)
+}
+
+func testAccCheckPingFederateSpAuthenticationPolicyContractMappingExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" || rs.Primary.ID == "0" {
+			return fmt.Errorf("No rule ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*pf.PfClient).SpAuthenticationPolicyContractMappings
+		result, _, err := conn.GetApcToSpAdapterMappingById(&pf.GetApcToSpAdapterMappingByIdInput{Id: rs.Primary.ID})
+
+		if err != nil {
+			return fmt.Errorf("Error: SpAuthenticationPolicyContractMapping (%s) not found", n)
+		}
+
+		if *result.SourceId != rs.Primary.Attributes["source_id"] {
+			return fmt.Errorf("Error: SpAuthenticationPolicyContractMapping response (%s) didnt match state (%s)", *result.SourceId, rs.Primary.Attributes["source_id"])
+		}
+
+		return nil
+	}
+}
+
+func Test_resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData(t *testing.T) {
+	cases := []struct {
+		Resource pf.ApcToSpAdapterMapping
+	}{
+		{
+			Resource: pf.ApcToSpAdapterMapping{
+				Id:                               String(""),
+				SourceId:                         String("foo"),
+				TargetId:                         String("foo"),
+				LicenseConnectionGroupAssignment: String("foo"),
+				DefaultTargetResource:            String("foo"),
+				AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+					"foo": {
+						Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+						Value:  String("foo"),
+					},
+				},
+				IssuanceCriteria: &pf.IssuanceCriteria{
+					ConditionalCriteria: &[]*pf.ConditionalIssuanceCriteriaEntry{
+						{
+							AttributeName: String("foo"),
+							Condition:     String("foo"),
+							ErrorResult:   String("foo"),
+							Source: &pf.SourceTypeIdKey{
+								Id:   String("foo"),
+								Type: String("foo"),
+							},
+							Value: String("foo"),
+						},
+					},
+					ExpressionCriteria: &[]*pf.ExpressionIssuanceCriteriaEntry{
+						{
+							ErrorResult: String("foo"),
+							Expression:  String("foo"),
+						},
+					},
+				},
+				AttributeSources: &[]*pf.AttributeSource{
+					{
+						LdapAttributeSource: pf.LdapAttributeSource{
+							AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+								"foo": {
+									Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+									Value:  String("foo"),
+								},
+							},
+							BaseDn: String("foo"),
+							BinaryAttributeSettings: map[string]*pf.BinaryLdapAttributeSettings{
+								"foo": {BinaryEncoding: String("foo")},
+							},
+							DataStoreRef: &pf.ResourceLink{
+								Id: String("foo"),
+							},
+							Description:         String("foo"),
+							Id:                  String("foo"),
+							MemberOfNestedGroup: Bool(true),
+							SearchFilter:        String("foo"),
+							SearchScope:         String("foo"),
+							Type:                String("LDAP"),
+						},
+						AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+							"foo": {
+								Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+								Value:  String("foo"),
+							},
+						},
+						DataStoreRef: &pf.ResourceLink{
+							Id: String("foo"),
+						},
+						Description: String("foo"),
+						Id:          String("foo"),
+						Type:        String("LDAP"),
+					},
+					{
+						JdbcAttributeSource: pf.JdbcAttributeSource{
+							AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+								"foo": {
+									Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+									Value:  String("foo"),
+								},
+							},
+							DataStoreRef: &pf.ResourceLink{
+								Id: String("foo"),
+							},
+							Description: String("foo"),
+							Filter:      String("foo"),
+							Id:          String("foo"),
+							Schema:      String("foo"),
+							Table:       String("foo"),
+							Type:        String("JDBC"),
+						},
+						AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+							"foo": {
+								Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+								Value:  String("foo"),
+							},
+						},
+						DataStoreRef: &pf.ResourceLink{
+							Id: String("foo"),
+						},
+						Description: String("foo"),
+						Id:          String("foo"),
+						Type:        String("JDBC"),
+					},
+					{
+						CustomAttributeSource: pf.CustomAttributeSource{
+							AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+								"foo": {
+									Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+									Value:  String("foo"),
+								},
+							},
+							DataStoreRef: &pf.ResourceLink{
+								Id: String("foo"),
+							},
+							Description: String("foo"),
+							FilterFields: &[]*pf.FieldEntry{
+								{
+									Name:  String("foo"),
+									Value: String("foo"),
+								},
+							},
+							Id:   String("foo"),
+							Type: String("CUSTOM"),
+						},
+						AttributeContractFulfillment: map[string]*pf.AttributeFulfillmentValue{
+							"foo": {
+								Source: &pf.SourceTypeIdKey{Type: String("foo"), Id: String("bar")},
+								Value:  String("foo"),
+							},
+						},
+						DataStoreRef: &pf.ResourceLink{
+							Id: String("foo"),
+						},
+						Description: String("foo"),
+						Id:          String("foo"),
+						Type:        String("CUSTOM"),
+					},
+				},
+			},
+		},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("tc:%v", i), func(t *testing.T) {
+
+			resourceSchema := resourcePingFederateSpAuthenticationPolicyContractMappingResourceSchema()
+			resourceLocalData := schema.TestResourceDataRaw(t, resourceSchema, map[string]interface{}{})
+			resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadResult(resourceLocalData, &tc.Resource)
+
+			if got := *resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData(resourceLocalData); !cmp.Equal(got, tc.Resource) {
+				t.Errorf("resourcePingFederateSpAuthenticationPolicyContractMappingResourceReadData() = %v", cmp.Diff(got, tc.Resource))
+			}
+		})
+	}
+}
