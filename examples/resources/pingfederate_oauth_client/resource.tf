afb415de6491abe44973306c557da9b3a01fb3b1
Iain Adams <iwarapter@hotmail.com>
2022-12-08 19:56:14 +0000

    fix examples

--- examples/resources/pingfederate_oauth_client/resource.tf
+++ examples/resources/pingfederate_oauth_client/resource.tf
@@ -1,18 +1,13 @@
 resource "pingfederate_oauth_client" "example" {
-  client_id   = "example"
-  name        = "example"
-  grant_types = ["CLIENT_CREDENTIALS"]
-
-  client_auth {
+  client_id                        = "example"
+  name                             = "example"
+  grant_types                      = ["CLIENT_CREDENTIALS"]
+  default_access_token_manager_ref = pingfederate_oauth_access_token_manager.example.id
+  client_auth = {
     secret = "super_top_secret"
     type   = "SECRET"
   }
-
-  default_access_token_manager_ref {
-    id = pingfederate_oauth_access_token_manager.example.id
-  }
-
-  oidc_policy {
+  oidc_policy = {
     grant_access_session_revocation_api = false
     logout_uris                         = ["https://logout", "https://foo"]
     ping_access_logout_capable          = true
